21c96def9a9005e7542ad53226558c9fe1a9ea6f
bokny <bokny@boknybook.home>
2017-02-19 16:05:19 +0000

    Initial Commit

---
+++ Slime ADT x/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Slime ADT x
+//
+//  Created by bokny on 19/02/2017.
+//  Copyright © 2017 bungo2dungo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
