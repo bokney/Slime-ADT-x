e39d041ea04ce9496bdd17d26e5b39b9d7c34255
bokny <bokny@boknybook.local>
2017-05-06 17:47:13 +0100

    hunger

--- Slime ADT x/main.c
+++ Slime ADT x/main.c
@@ -13,47 +13,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "slime_list_nodes_access.h"
+#include "slime_stacks.h"
 #include "slime_lists.h"
-#include "slime_priority_lists.h"
-#include "test_structures.h"
+#include "fruit.h"
 
 extern ll_node *spare;
 
+slimeStack *fruitStack = NULL;
+
 int main(int argc, char **argv) {
     
-    srand(time(NULL));
+    printf("Starting up\n");
+    printf("%i spare nodes\n", llCount(spare));
     
-    pl_container *testPL = priorityListCreate();
+    printf("Creating fruitStack\n");
+    fruitStack = slimeStackCreate(createFruit, initFruit, destroyFruit);
     
-    printf("Adding some numbers to the priority list:\n");
-    unsigned int amt = 4 + (rand() % 8);
-    for (int i = 0; i != amt; i++) {
-        int r = rand() % 8;
-        if (!(i % 8)) r = 0;
-        printf("%i ", r);
-        priorityListAmend(testPL, r, r);
+    printf("Adding fruit\n");
+    fruit *testFruit = slimeStackPush(fruitStack);
+    testFruit->name = "piss orange";
+    testFruit->weight = 99;
+    testFruit = slimeStackPush(fruitStack);
+    testFruit->name = "gentle fellow peach";
+    testFruit->weight = 14;
+    testFruit = slimeStackPush(fruitStack);
+    testFruit->name = "dingo buttock";
+    testFruit->weight = 37;
+    
+    printf("Fruit added\n");
+    printf("%i spare nodes\n", llCount(spare));
+
+    printf("Popping fruit\n");
+    fruit *provenFruit = slimeStackPop(fruitStack);
+    while (provenFruit != NULL) {
+        printf("%s, weighing in at %i chungos\n", provenFruit->name, provenFruit->weight);
+        provenFruit = slimeStackPop(fruitStack);
     }
-    
-    ll_node *sorted = priorityListDiscardContainer(testPL);
-    
-    printf("\nReading out the sorted numbers:\n");
-    
-    ll_node *traversal = sorted;
-    while (traversal != NULL) {
-        printf("%i ", ll_nodeGetData(traversal));
-        traversal = ll_nodeGetNext(traversal);
-    }
-    printf("\n");
-    
-    printf("Destroying the sorted list!\n");
-    llDestroy(&sorted);
-    
-    printf("Detected %i spare nodes!\nDestroying all spare!\n", llCount(spare));
-    
-    ll_nodeDestroyAllSpare();
-    
-    printf("Detected %i spare nodes!\n", llCount(spare));
-    
+    printf("%i spare nodes\n", llCount(spare));
+
+    printf("Destroying fruitStack\n");
+    slimeStackDestroy(fruitStack);
+    printf("%i spare nodes\n", llCount(spare));
+    printf("End\n");
     return 0;
 }
